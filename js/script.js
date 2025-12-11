8f66ea3610d44b3d02ed9a57d400ca20af2e226b
Abdurrahman-code19 <abefeori@gmail.com>
2025-12-11 19:43:35 +0100

    calc

--- js/script.js
+++ js/script.js
@@ -26,5 +26,34 @@ function calculate() {
         let result = n1 + n2
         displayEl.innerText = result
     }
-    
+
+    if (operator === "-") {
+        let result = n1 - n2
+        displayEl.innerText = result
+    }
+
+    if (operator === "×") {
+        let result = n1 * n2
+        displayEl.innerText = result
+    }
+
+    if (operator === "÷") {
+        if (n2 === 0) {
+            displayEl.innerText = "SyntaxError"
+        } else {
+            let result = n1 / n2
+            displayEl.innerText = result
+        }
+    }
+}
+
+function clearAll() {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    displayEl.innerText = "0";
+}
+
+function clearEntry() {
+    displayEl.innerText = displayEl.innerText.slice(0, -1);
 }
